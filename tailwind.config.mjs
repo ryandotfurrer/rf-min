ae8ee39bee259067bf2656458e3644b9999c6a0b
Ryan Furrer <ryandotfurrer@gmail.com>
2024-05-27 21:13:04 -0400

    Begin revising styles and certain layouts

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,19 +2,19 @@
 export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
-    fontFamily: {
-      sans: ["Inter", "Arial", "sans-serif"],
-    },
-    fontSize: {
-      xs: "clamp(0.7813rem, 0.9477rem + -0.1734vi, 0.8784rem);",
-      sm: "clamp(0.9372rem, 0.937rem + 0.0005vi, 0.9375rem);",
-      base: "clamp(1rem, 0.9107rem + 0.2232vi, 1.125rem);", // p
-      lg: "clamp(1.067rem, 0.8649rem + 0.5054vi, 1.35rem);",
-      xl: "clamp(1.1385rem, 0.7946rem + 0.8598vi, 1.62rem);",
-      "2xl": "clamp(1.2148rem, 0.6939rem + 1.3022vi, 1.944rem);", // h3
-      "3xl": "clamp(1.2962rem, 0.5557rem + 1.8511vi, 2.3328rem);", // h2
-      "4xl": "clamp(1.383rem, 0.3713rem + 2.5292vi, 2.7994rem);", // h1
-    },
+    // fontFamily: {
+    //   sans: ["Inter", "Arial", "sans-serif"],
+    // },
+    // fontSize: {
+    //   xs: "clamp(0.7813rem, 0.9477rem + -0.1734vi, 0.8784rem);",
+    //   sm: "clamp(0.9372rem, 0.937rem + 0.0005vi, 0.9375rem);",
+    //   base: "clamp(1rem, 0.9107rem + 0.2232vi, 1.125rem);", // p
+    //   lg: "clamp(1.067rem, 0.8649rem + 0.5054vi, 1.35rem);",
+    //   xl: "clamp(1.1385rem, 0.7946rem + 0.8598vi, 1.62rem);",
+    //   "2xl": "clamp(1.2148rem, 0.6939rem + 1.3022vi, 1.944rem);", // h3
+    //   "3xl": "clamp(1.2962rem, 0.5557rem + 1.8511vi, 2.3328rem);", // h2
+    //   "4xl": "clamp(1.383rem, 0.3713rem + 2.5292vi, 2.7994rem);", // h1
+    // },
     extend: {},
   },
   plugins: [],
